43a6a00e6ead64f215ab09aa675f8b38701cc124
Kevin Talmadge <kevin@eluv.io>
2019-02-08 12:33:45 -0800

    Show to and from addresses and attempt to parse bytes32 strings in blockchain event logs

--- src/clients/Fabric.js
+++ src/clients/Fabric.js
@@ -853,7 +853,7 @@ const Fabric = {
   },
 
   ContractEvents: ({contractAddress, abi, fromBlock, toBlock}) => {
-    return client.ContractEvents({contractAddress, abi, fromBlock, toBlock});
+    return client.ContractEvents({contractAddress, abi, fromBlock, toBlock, includeSender: true});
   },
 
   WithdrawContractFunds: ({contractAddress, abi, ether}) => {
@@ -861,7 +861,7 @@ const Fabric = {
   },
 
   GetBlockchainEvents: ({toBlock, fromBlock, count=10}) => {
-    return client.Events({toBlock, fromBlock, count});
+    return client.Events({toBlock, fromBlock, count, includeSender: true});
   },
 
   /* Naming */
