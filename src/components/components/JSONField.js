3b3ff7ec362388096deca7b0edec3fb314f533ba
elv-zenia <91760982+elv-zenia@users.noreply.github.com>
2021-10-08 13:35:52 -0700

    Make content object version metadata copyable ; Diff case when no previous version (#6)
    * Make content object version metadata copyable ; Diff case when no previous version

--- src/components/components/JSONField.js
+++ src/components/components/JSONField.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {Tabs, TraversableJson} from "elv-components-js";
 import Diff from "../components/Diff";
+import {Copyable} from "./Copyable";
 
 const JSONField = ({json, diffJson, DiffComponent}) => {
   typeof json === "string" ? json = JSON.parse(json || "{}") : json;
@@ -27,7 +28,11 @@ const JSONField = ({json, diffJson, DiffComponent}) => {
   let content;
   switch(viewType) {
     case "raw":
-      content = <pre className="content-object-data">{JSON.stringify(json, null, 2)}</pre>;
+      content = (
+        <pre className="content-object-data">
+          <Copyable copy={JSON.stringify(json, null, 2)} className="raw-data">{JSON.stringify(json, null, 2)}</Copyable>
+        </pre>
+      );
       break;
     case "diff":
       if(diffJson) {
