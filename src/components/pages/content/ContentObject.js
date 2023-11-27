45e63a678a239930301237baf200c576cb973b7c
elv-zenia <zenia.villa@eluv.io>
2023-11-27 14:30:12 -0800

    Missing aync/await

--- src/components/pages/content/ContentObject.js
+++ src/components/pages/content/ContentObject.js
@@ -978,9 +978,9 @@ class ContentObject extends React.Component {
         <ContentObjectGroups
           currentPage="contentObject"
           showGroupPermissionsButton={this.props.objectStore.object.isOwner || (this.props.objectStore.object.isNormalObject && this.props.objectStore.object.permission !== "owner" && this.props.objectStore.object.canEdit)}
-          LoadGroupPermissions={() => {
-            this.props.objectStore.ContentObjectGroupPermissions({objectId: this.props.objectStore.objectId});
-            this.props.objectStore.ContentObjectUserPermissions({
+          LoadGroupPermissions={async () => {
+            await this.props.objectStore.ContentObjectGroupPermissions({objectId: this.props.objectStore.objectId});
+            await this.props.objectStore.ContentObjectUserPermissions({
               objectId: this.props.objectStore.objectId,
               libraryId: this.props.objectStore.libraryId
             });
